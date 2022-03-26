c6a1c4543f83ceb4a582f062d48a50d95a64ed91
wildpl <wildpl@mail.ru>
2022-03-26 20:41:20 +0300

    Java Core lvl.1 HomeWork_3 - 3

--- src/main/java/ru/gb/java_core/level_1/HomeWork_3.java
+++ src/main/java/ru/gb/java_core/level_1/HomeWork_3.java
@@ -15,19 +15,38 @@ public class HomeWork_3 {
 //        System.out.println("-----------");
 
         // 2
-        int[] arrFill = new int[100];
-        arrayFullFill(arrFill);
+//        int[] arrFill = new int[100];
+//        arrayFullFill(arrFill);
+//        System.out.println("-----------");
+
+        // 3
+        int[] arrZeroOne = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        changeInArrayXTwo(arrZeroOne);
         System.out.println("-----------");
-
-
 
 
 
     }
 
-    private static void arrayFullFill(int [] arr) {
+    // 3 - проверить массив и умножить на 2 значения меньше 6
+    private static void changeInArrayXTwo(int[] arr) {
+        System.out.println("Before:\t" + Arrays.toString(arr));
+
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+        System.out.println("After:\t" + Arrays.toString(arr));
+    }
+
+    // 2 - заполнить пустой массив размером 100 значениями 1..100
+    private static void arrayFullFill(int[] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = i + 1;
+//        }
+        for (int i = 0, j = 1; i < arr.length; i++, j++) {
+            arr[i] = j;
         }
         System.out.println(Arrays.toString(arr));
     }
