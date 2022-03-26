9c9f3cb440d0b8cb4455b2c2e1170d5b52bc92f3
wildpl <wildpl@mail.ru>
2022-03-26 20:15:50 +0300

    Java Core lvl.1 HomeWork_3 - 1

--- src/main/java/ru/gb/java_core/level_1/HomeWork_3.java
+++ src/main/java/ru/gb/java_core/level_1/HomeWork_3.java
@@ -1,14 +1,38 @@
 package ru.gb.java_core.level_1;
 
+import java.util.Arrays;
+
 public class HomeWork_3 {
 
     public static void main(String[] args) {
 
-
+        // 1
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        changeInArrayZeroOne(arr);
 
     }
 
+    // 1 - инвертировать в массиве (0 в 1) и (1 в 0)
+    private static void changeInArrayZeroOne(int[] arr) {
+        System.out.println("Before:\t" + Arrays.toString(arr));
 
+        for (int i = 0; i < arr.length; i++) {
+//            switch (arr[i]) {
+//                case 0:
+//                    arr[i] = 1;
+//                    break;
+//                case 1:
+//                    arr[i] = 0;
+//                    break;
+//            }
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        System.out.println("After:\t" + Arrays.toString(arr));
+    }
 
 
 }
