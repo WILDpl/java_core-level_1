16472a727220f6ccbc7439565309db5a07304bf4
wildpl <wildpl@mail.ru>
2022-04-06 22:05:11 +0300

    Java Core lvl.1 Homework_6 - OOP

---
+++ src/main/java/ru/gb/java_core/level_1/Animal.java
@@ -0,0 +1,24 @@
+package ru.gb.java_core.level_1;
+
+public abstract class Animal extends Object {
+
+    protected String name;
+//    protected int distance;
+    int countAnimal;
+
+    public Animal() {
+        System.out.println("Animal born!");
+    }
+
+    public Animal(String name) {
+        this();
+        this.name = name;
+    }
+
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
+
+}
+
+
